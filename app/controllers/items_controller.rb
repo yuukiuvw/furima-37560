7c5fb6806efd7d126c0eed992113853382649a50
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-03-09 11:52:46 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
+    @items = Item.includes(:user)
   end
 
-  # def new
-  #   @item = Item.new
-  # end
+  def new
+    @item = Item.new
+  end
 
-  # def create
-  #   @item = Item.new(item_params)
-  #   if @item.save
-  #     redirect_to root_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-#  private 
-# def item_params
-#   params.require(:item).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-# end
+ private 
+def item_params
+  params.require(:item).permit(:image,:product_name,:explanation,:price,:category_id,:product_condition_id,:shipping_charge_id,:prefecture_id,:days_to_ship_id).merge(user_id: current_user.id)
+end
 
 end
