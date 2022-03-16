10e347b505dc40970bd0770755ae2a16ad7326ad
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-03-16 14:19:50 +0900

    S3実装中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :birthday, :name, :surname, :name_pseudonym, :surname_pseudonym])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :birthday, :name, :surname, :name_pseudonym, :surname_pseudonym])
+  end
+
+  def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]      end  
   end
 end
